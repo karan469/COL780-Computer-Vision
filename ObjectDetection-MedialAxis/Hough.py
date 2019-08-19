883be03516c84ee0737e1dafde23d5afb6e1a5cd
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-08-19 15:30:29 +0530

    Update and rename third.py to Hough.py

--- ObjectDetection-MedialAxis/Hough.py
+++ ObjectDetection-MedialAxis/Hough.py
@@ -9,10 +9,13 @@ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 # fgbg = cv2.createBackgroundSubtractorGMG()
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
-kernel = np.ones((5,5),np.uint8)
+erodeKernel = np.ones((5,5),np.uint8)
+dilateKernel = np.ones((7,1),np.uint8)
+
 # more sizeof kernel is making less noise but after a reach, its reducing main foreground too
 # element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
 
+thetaPrev = 0
 
 while(1):
     ret, frame = cap.read()
@@ -25,8 +28,10 @@ while(1):
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
     
     # It erodes away the boundaries of foreground object | A pixel in the original image (either 1 or 0) will be considered 1 only if all the pixels under the kernel is 1, otherwise it is eroded (made to zero).
-    erosion = cv2.erode(frame,kernel,iterations=1)
-
+    erosion = cv2.erode(fgmask,erodeKernel,iterations=2)
+    
+    #erosion = cv2.dilate(fgmask, kernel, iterations = 1)
+    
     #for boundry derivative - no need coz using canny boundry detector
     # sobelx = cv2.Sobel(fgmask,cv2.CV_64F,1,0,ksize=3)
     # sobely = cv2.Sobel(fgmask,cv2.CV_64F,0,1,ksize=3)
@@ -36,14 +41,16 @@ while(1):
     edges = cv2.Canny(fgmask,50,150,apertureSize = 3)
     
     # This will make the lines thicker which will help fit the Hough lines better | This is practically not working
-    # edges = cv2.dilate(edges,kernel,iterations = 1)
-    
-    lines = cv2.HoughLines(edges,1,np.pi/180,150) # last param is the threshold
-    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1.2, 100)
+    #edges = cv2.dilate(edges,dilateKernel,iterations = 1)
 
+    lines = cv2.HoughLines(edges,1,np.pi/180,100) # last param is the threshold
+    #linesP = cv2.HoughLines(edges, 1, np.pi/180, 100, 5, 10)
+
+    # dst = cv2.cornerHarris(fgmask, 2, 3, 0.04)
+    # print(dst[0][0])
     if lines is not None:
         for line in lines:
-            for rho,theta in line:
+            for rho,theta in lines[0]:
                 a = np.cos(theta)
                 b = np.sin(theta)
                 x0 = a*rho
@@ -54,19 +61,7 @@ while(1):
                 y2 = int(y0 - 1000*(a))
 
                 # now the line is generated on to the original colored frame
-                cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),2)
-
-    # if circles is not None:
-    #     circles = np.round(circles[0, :]).astype("int")
-    #     for (x, y, r) in circles:
-    #         # draw the circle in the output image, then draw a rectangle
-    #         # corresponding to the center of the circle
-    #         cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-            
-    #         #rectangle for center
-    #         #cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-
-
+                cv2.line(frame,(x1,y1),(x2,y2),(255,40,10),2)
 
     cv2.imshow('frame',frame)
     k = cv2.waitKey(1) & 0xff
