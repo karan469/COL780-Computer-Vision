c5b95458021f21a2bafccfe3eba558b167adf4c4
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-09-30 20:46:35 +0530

    All multiple images done

--- Stitch_Panorama/v2.py
+++ Stitch_Panorama/v2.py
@@ -6,69 +6,79 @@ import math
 import os
 import glob
 
-orb = cv2.ORB_create(nfeatures = 100000, scoreType=cv2.ORB_FAST_SCORE) # Initiate SIFT detector
+def multiplyMatrix(X,Y):
+	result = [[0 for x in range(3)] for y in range(3)]
+	for i in range(len(X)):  
+	   for j in range(len(Y[0])):  
+		   for k in range(len(Y)):  
+			   result[i][j] += X[i][k] * Y[k][j]
+	return result
 
-# img1 = cv2.imread(sys.argv[1],0) # queryImage-1
-# img2 = cv2.imread(sys.argv[2],0) # queryImage-2
+# allImages = ['./dir1/214.jpg','./dir1/213.jpg','./dir1/212.jpg']
+# allImages = ['./dir2/14.jpeg','./dir2/13.jpeg','./dir2/11.jpeg']
+allImages = ['./4.jpg','./3.jpg','./2.jpg', './1.jpg']
 
-# kp1, des1 = orb.detectAndCompute(img1, None)
-# kp2, des2 = orb.detectAndCompute(img2, None)
-
-# -------------------------------------------------------------------------------
-# user_input = raw_input("Enter the path of your file: ")
-# assert os.path.exists(user_input), "I did not find the file at, "+str(user_input)
-# inn = user_input + '*.jpg'
-# allImages = glob.glob(inn)
-# sorted(allImages, key = lambda x: x.split(user_input)[1].split('.jpg')[0])
-allImages = ['./dir2/1.jpeg','./dir2/2.jpeg','./dir2/3.jpeg', './dir2/4.jpeg']
-# print(imgreads)
 imgreads = []
 for singleImg in allImages:
 	print(singleImg)
 	imgreads.append(cv2.imread(singleImg,0))
 
+orb = cv2.ORB_create(nfeatures = 100000, scoreType=cv2.ORB_FAST_SCORE) # Initiate SIFT detector
 kp = []
 des = []
 for img in imgreads:
 	kp.append(orb.detectAndCompute(img, None)[0])
 	des.append(orb.detectAndCompute(img, None)[1])
 
-#--------------------------------------------------------------------------------	
-
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+dmatchArr = []
+for i in range(0, len(imgreads)-1):
+	dmatchArr.append(sorted(bf.match(des[i],des[i+1]), key = lambda x: x.distance))
 
-allmatches = []
-alldmatches = []
-for i in range(0,len(des)-2):
-	allmatches.append(bf.match(des[i],des[i+1]))
-	alldmatches.append(sorted(bf.match(des[i],des[i+1]), key = lambda x: x.distance))
+srcNdes = []
+MNmask = []
+hNw = []
+ptsArr = []
+# resArr = []
+dstArr = []
+for i in range(0, len(imgreads)-1):
+	src_pts = np.float32([kp[i][m.queryIdx].pt for m in dmatchArr[i]]).reshape(-1,1,2)
+	des_pts = np.float32([kp[i+1][m.trainIdx].pt for m in dmatchArr[i]]).reshape(-1,1,2)
+	srcNdes.append(( src_pts, des_pts))
+	
+	M, mask = cv2.findHomography(src_pts, des_pts, cv2.RANSAC,5.0)
+	MNmask.append((M, mask))
+	
+	h,w = imgreads[i].shape[:2]
+	hNw.append((h,w))
+	
+	pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+	ptsArr.append(pts)
+	
+	dst = cv2.perspectiveTransform(pts,M)
+	# print(dst)
+	dstArr.append(dst)
+	
+	imgreads[i+1] = cv2.polylines(imgreads[i+1], [np.int32(dst)], True, (0,0,255), 1, cv2.LINE_AA)
+	res1 = cv2.drawMatches(imgreads[i], kp[i], imgreads[i+1], kp[i+1], dmatchArr[i][:20],None,flags=2)
 
-matches = bf.match(des[0], des[1])
-dmatches = sorted(matches, key = lambda x:x.distance)
+dst = []
+ded =  cv2.warpPerspective(imgreads[0],MNmask[0][0],(imgreads[1].shape[1] + imgreads[0].shape[1], imgreads[1].shape[0]))
+for i in range(0, len(imgreads)-1):
+	if i>0:
+		ded = cv2.warpPerspective(ded,MNmask[i][0],(imgreads[i+1].shape[1] + ded.shape[1], imgreads[i+1].shape[0]))
+	
+	ded[0:imgreads[i+1].shape[0], 0:imgreads[i+1].shape[1]] = imgreads[i+1]
+	dst.append(ded)
+	print(ded)
 
+# print(len(dst))
+# print(dst[0])
+ded[0:imgreads[len(imgreads)-1].shape[0], 0:imgreads[len(imgreads)-1].shape[1]] = imgreads[len(imgreads)-1]
+plt.subplot(122),plt.imshow(ded),plt.title('Warped Image')
 
-## extract the matched keypoints
-src_pts  = np.float32([kp[0][m.queryIdx].pt for m in dmatches]).reshape(-1,1,2)
-dst_pts  = np.float32([kp[1][m.trainIdx].pt for m in dmatches]).reshape(-1,1,2)
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-
-# print(M)
-
-h,w = imgreads[0].shape[:2]
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
-
-imgreads[1] = cv2.polylines(imgreads[1], [np.int32(dst)], True, (0,0,255), 1, cv2.LINE_AA)
-
-res = cv2.drawMatches(imgreads[0], kp[0], imgreads[1], kp[1], dmatches[:20],None,flags=2)
-
-dst = cv2.warpPerspective(imgreads[0],M,(imgreads[1].shape[1] + imgreads[0].shape[1], imgreads[1].shape[0]))
-plt.subplot(122),plt.imshow(dst),plt.title('Warped Image')
-dst[0:imgreads[1].shape[0], 0:imgreads[1].shape[1]] = imgreads[1]
-cv2.imwrite('output.jpg',dst)
-plt.imshow(dst)
+cv2.imwrite('./dir1/dir1output.jpg',dst[len(dst)-1])
+plt.imshow(ded)
 plt.show()
 
-
 cv2.waitKey();cv2.destroyAllWindows()
-
