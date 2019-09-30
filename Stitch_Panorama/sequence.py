1199eb2240b0a4a117968106f941183ccfe7098e
Shreya Sharma <34606101+ShreyaSharma99@users.noreply.github.com>
2019-09-30 23:13:31 +0530

    Update sequence.py

--- Stitch_Panorama/sequence.py
+++ Stitch_Panorama/sequence.py
@@ -29,6 +29,8 @@ orb = cv2.ORB_create(nfeatures = 100000, scoreType=cv2.ORB_FAST_SCORE) # Initiat
 
 print("Enter the image folder name: ")
 images = load_images_from_folder(str(input()))
+print("First img:")
+first = int(input())
 index=0
 kp_temp,des_temp = orb.detectAndCompute(images[index], None)
 kp = [kp_temp]
@@ -263,18 +265,22 @@ def find_left(index):
 ### calling functions to complete image_right and image_left matrices
 
 
-i = 0
-for img in images:
+i = first-1
+for img in range(0,len(images)-1):
 	print("i ",i)
 	if images_right[i] == -1:
 		find_right(i)
-	i +=1
-i=0
-for img in images:
-	print("i ",i)
-	if images_left[i] == -1:
-		find_left(i)
-	i +=1
+		i = images_right[i]
+	else:
+		i +=1
+# i=0
+# for img in images:
+# 	print("i ",i)
+# 	if images_left[i] == -1:
+# 		find_left(i)
+# 		i = images_left[i]
+# 	else:
+# 		i +=1
 
 # match_index = find_max(match_temp,index)
 # get_order(index,match_index)
@@ -284,13 +290,13 @@ print(images_left)
 ###### order images ######
 i=0
 start = -1
-for val in images_left:
-	if images_left[i] == -1:
-		start = i
-		break
-	i +=1
-sorted_images = [images[start]]
-i_next = start
+# for val in images_left:
+# 	if images_left[i] == -1:
+# 		start = i
+# 		break
+# 	i +=1
+sorted_images = [images[0]]
+i_next = 0
 print(i_next)
 while images_right[i_next]!=-1:
 	i_next = images_right[i_next]
@@ -305,70 +311,6 @@ while images_right[i_next]!=-1:
 
 # findHomography_(img1, img2)
 ###################################################################################################################################
-def diffArray(des1, des2):
-	res = []
-	for i in range(len(des1)):
-			res.append(des1[i]-des2[i])
-	return res
-
-
-def squareArray(des1):
-	res = 0
-	for i in range(len(des1)):
-		res = res + des1[i]*des1[i]
-	return math.sqrt(res)
-
-# print((diffArray(des1,des2)))
-# print(np.diff(des1,des2))
-
-# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-# matches1 = bf.knnMatch(des1, des2)
-# dmatches1 = sorted(matches1, key = lambda x:x.distance)
-# min_match1 = matches1[0].distance
-# matches2 = bf.match(des2, des1)
-# dmatches2 = sorted(matches2, key = lambda x:x.distance)
-# min_match2 = matches2[0].distance
-
-# if min_match2 < min_match1:
-# 	image_temp = des1
-# 	des1 = des2
-# 	des2 = image_temp
-# 	kp_temp = kp1
-# 	kp1 = kp2
-# 	kp2 = kp_temp
-# 	dmatches1 = dmatches2
-
-
-
-## extract the matched keypoints
-src_pts  = np.float32([kp1[m.queryIdx].pt for m in dmatches1]).reshape(-1,1,2)
-dst_pts  = np.float32([kp2[m.trainIdx].pt for m in dmatches1]).reshape(-1,1,2)
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-
-print(M)
-
-h,w = img1.shape[:2]
-# print(h,w)
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
-
-# print(pts)
-
-img2 = cv2.polylines(img2, [np.int32(dst)], True, (0,0,255), 1, cv2.LINE_AA)
-# cv2.imshow("found", img2)
-
-res = cv2.drawMatches(img1, kp1, img2, kp2, dmatches1[:20],None,flags=2)
-
-cv2.imshow("orb_match", res);
-
-dst = cv2.warpPerspective(img1,M,(img2.shape[1] + img1.shape[1], img2.shape[0]))
-plt.subplot(122),plt.imshow(dst),plt.title('Warped Image')
-# plt.show() # until this img2 is warped and ready to be stitched
-# plt.figure()
-dst[0:img2.shape[0], 0:img2.shape[1]] = img2
-cv2.imwrite('output.jpg',dst)
-plt.imshow(dst)
-plt.show()
 
 
 cv2.waitKey();cv2.destroyAllWindows()
