cbda4946daa804f0a73e87481fff0dd89f6b6e93
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-10-31 17:47:45 +0530

    Update gesturecnn.py

--- music_gesture_control/gesturecnn.py
+++ music_gesture_control/gesturecnn.py
@@ -1,43 +1,69 @@
-# Importing all necessary libraries 
-from keras.preprocessing.image import ImageDataGenerator 
-from keras.models import Sequential 
-from keras.layers import Conv2D, MaxPooling2D 
-from keras.layers import Activation, Dropout, Flatten, Dense 
-from keras import backend as K
+# Ive used all frames of video for generating training or testing data | Take 10th frame or so
+# FAST_RUN should be true since we have small no. of data | Meaning: less no. of epochs
+# Ensure shuffling of testing data
+import numpy as np
+import pandas as pd 
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-import numpy
+import random
+import os
 
-img_width, img_height = 50, 50
+FAST_RUN = True
+IMAGE_WIDTH=50
+IMAGE_HEIGHT=50
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
 
-def resize(image, width, height=None):
-    """
-    Args:
-        1. image:   Image which has to be resized.
-        2. width:   New width for the image.
-        3. height:  New height for the image.
-    """
-    h,w,channels = image.shape
-    if not height:
-        ratio = float(width) / w
-        height = int(ratio*h)
+filenames = os.listdir("./train")
+categories = []
+for filename in filenames:
+    category = filename.split('.')[0]
+    if category == 'next':
+        categories.append(1)
+    else:
+        categories.append(0)
 
-    resized = cv2.resize(image, (width, height))
-    return resized
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories
+})
+df.head()
+df.tail()
+df['category'].value_counts().plot.bar()
+sample = random.choice(filenames)
+image = load_img("./train/"+sample)
+plt.imshow(image)
 
-train_data_dir = './train'
-validation_data_dir = './validation'
-nb_train_samples =2524
-nb_validation_samples = 1919
-epochs = 10
-batch_size = 16
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
 
-if K.image_data_format() == 'channels_first': 
-    input_shape = (3, img_width, img_height) 
-else: 
-    input_shape = (img_width, img_height, 3) 
+model = Sequential()
 
-model = Sequential() 
-model.add(Conv2D(32, (2, 2), input_shape=input_shape)) 
+# model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+
+# model.add(Conv2D(128, (3, 3), activation='relu'))
+# model.add(BatchNormalization())
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+
+# model.add(Flatten())
+# model.add(Dense(512, activation='relu'))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.5))
+# model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
+
+#-----
+model.add(Conv2D(32, (2, 2), input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS))) 
 model.add(Activation('relu')) 
 model.add(MaxPooling2D(pool_size=(2, 2))) 
   
@@ -53,49 +79,130 @@ model.add(Flatten())
 model.add(Dense(64)) 
 model.add(Activation('relu')) 
 model.add(Dropout(0.5)) 
-model.add(Dense(1)) 
+model.add(Dense(2)) 
 model.add(Activation('sigmoid')) 
 
-model.compile(loss='binary_crossentropy', 
-              optimizer='rmsprop', 
-              metrics=['accuracy'])
+# model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-train_datagen = ImageDataGenerator( 
-                rescale = 1. / 255, 
-                 shear_range = 0.2, 
-                  zoom_range = 0.2, 
-            horizontal_flip = True) 
-  
-test_datagen = ImageDataGenerator(rescale = 1. / 255) 
-  
-train_generator = train_datagen.flow_from_directory(train_data_dir, 
-                              target_size =(img_width, img_height), 
-                     batch_size = batch_size, class_mode ='binary') 
-  
-validation_generator = test_datagen.flow_from_directory( 
-                                    validation_data_dir, 
-                   target_size =(img_width, img_height), 
-          batch_size = batch_size, class_mode ='binary') 
-  
-history = model.fit_generator(train_generator, 
-    steps_per_epoch = nb_train_samples // batch_size, 
-    epochs = epochs, validation_data = validation_generator, 
-    validation_steps = nb_validation_samples // batch_size) 
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+model.summary()
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+earlystop = EarlyStopping(patience=10)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=2, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
 
-model.save_weights('model_saved.h5') 
+callbacks = [earlystop, learning_rate_reduction]
+df["category"] = df["category"].replace({0: 'pause', 1: 'next'})
+
+# 2 - cross validation
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+
+train_df['category'].value_counts().plot.bar()
+validate_df['category'].value_counts().plot.bar()
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+batch_size=30
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+train_generator = train_datagen.flow_from_dataframe(
+    train_df, 
+    "./train/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df, 
+    "./train/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+) 
+
+# plt.figure(figsize=(12, 12))
+# for i in range(0, 15):
+#     plt.subplot(5, 3, i+1)
+#     for X_batch, Y_batch in example_generator:
+#         image = X_batch[0]
+#         plt.imshow(image)
+#         break
+# plt.tight_layout()
+# plt.show()
+
+epochs=1 if FAST_RUN else 10
+history = model.fit_generator(
+    train_generator, 
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate//batch_size,
+    steps_per_epoch=total_train//batch_size,
+    callbacks=callbacks
+)
+
+model.save_weights("model.h5")
+
+# visualize training
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+ax1.plot(history.history['loss'], color='b', label="Training loss")
+ax1.plot(history.history['val_loss'], color='r', label="validation loss")
+ax1.set_xticks(np.arange(1, epochs, 1))
+ax1.set_yticks(np.arange(0, 1, 0.1))
+
+ax2.plot(history.history['accuracy'], color='b', label="Training accuracy")
+ax2.plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+ax2.set_xticks(np.arange(1, epochs, 1))
+
+legend = plt.legend(loc='best', shadow=True)
+plt.tight_layout()
+# plt.show()
+
+test_filenames = os.listdir('./test1')
+test_df = pd.DataFrame({
+    'filename': test_filenames
+})
+nb_samples = test_df.shape[0]
+
+test_gen = ImageDataGenerator(rescale=1./255)
+test_generator = test_gen.flow_from_dataframe(
+    test_df, 
+    "./test1/", 
+    x_col='filename',
+    y_col=None,
+    class_mode=None,
+    target_size=IMAGE_SIZE,
+    batch_size=batch_size,
+    shuffle=False
+)
+
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/batch_size))
+test_df['category'] = np.argmax(predict, axis=-1)
+label_map = dict((v,k) for k,v in train_generator.class_indices.items())
+test_df['category'] = test_df['category'].replace(label_map)
+# test_df['category'] = test_df['category'].replace({ 'next': 1, 'pause': 0 })
+test_df['category'].value_counts().plot.bar()
+
+sample_test = test_df.head(18)
+print('------------------Testing Data---------------------')
+print(sample_test)
