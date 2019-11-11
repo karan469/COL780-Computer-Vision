481902bb4818c70d24846060390a391766b00cc7
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-11-11 19:26:23 +0530

    Update generator.py

--- music_gesture_control/generator.py
+++ music_gesture_control/generator.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('next1.webm')
 fgbg = cv2.createBackgroundSubtractorMOG2() 
 count = 1
-
+kernel = np.ones((3,3),np.uint8)/9
 while True:
 	# read the current frame
 	ret, frame = cap.read()
@@ -13,19 +13,28 @@ while True:
 		print ("Unable to capture video")
 		break 
 
-	fgmask = fgbg.apply(frame) 
+	# fgmask = fgbg.apply(frame) 
+	# fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+	# fgmask = cv2.medianBlur(fgmask, 3)
 	# cv2.imshow('fgmask', frame)
-	cv2.imshow('frame', fgmask)
-	if count%30==1:
-		# cv2.imshow("Frame", frame)
-		cnt = (int)(count/30)
-		print("./binImages/next." + (str)(cnt) + ".png")
+	
+	# cv2.imshow('frame', fgmask)
+
+	cv2.imwrite("./binImages/next." + (str)(cnt) + ".png", frame)
+	print("./binImages/next." + (str)(cnt) + ".png")
+
+	# if count%30==1:
+	# 	# cv2.imshow("Frame", frame)
+	# 	cnt = (int)(count/30)
+	# 	print("./binImages/next." + (str)(cnt) + ".png")
 		
-		# cv2.imwrite("./binImages/next." + (str)(cnt) + ".png", frame)
-	elif count>270:
-		break
-	# cv2.imshow("Frame", frame)
+	# 	cv2.imwrite("./binImages/next." + (str)(cnt) + ".png", frame)
+	
+	# elif count>270:
+	# 	break
+	
 	count += 1
+	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
